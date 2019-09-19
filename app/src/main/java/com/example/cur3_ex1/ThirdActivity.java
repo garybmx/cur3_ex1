e3950bbd7a3a78b80119dac3972fecf12cc846b3
Igor U <garymtb@yandex.ru>
2019-09-19 21:55:08 +0300

    ex2

---
+++ app/src/main/java/com/example/cur3_ex1/ThirdActivity.java
@@ -0,0 +1,17 @@
+package com.example.cur3_ex1;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class ThirdActivity extends AppCompatActivity {
+    Button sendButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+    }
+}
